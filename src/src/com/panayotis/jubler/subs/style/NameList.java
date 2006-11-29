0a3625498054bcca14eb9f0962910b2416dd6fa5
teras <teras@localhost>
2006-11-29 17:05:59 +0200

    [svn r67] Fix __PROJECT__ error

--- src/src/com/panayotis/jubler/subs/style/NameList.java
+++ src/src/com/panayotis/jubler/subs/style/NameList.java
@@ -3,20 +3,20 @@
  *
  * Created on 3 Σεπτέμβριος 2005, 6:51 μμ
  *
- * This file is part of __PROJECTNAME__.
+ * This file is part of Jubler.
  *
- * __PROJECTNAME__ is free software; you can redistribute it and/or modify
+ * Jubler is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2.
  *
  *
- * __PROJECTNAME__ is distributed in the hope that it will be useful,
+ * Jubler is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with __PROJECTNAME__; if not, write to the Free Software
+ * along with Jubler; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
