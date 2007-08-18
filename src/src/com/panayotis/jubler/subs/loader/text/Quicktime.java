1cfa97586adcd533691dc289bc0b13a3464e724a
teras <teras@localhost>
2007-08-18 22:35:42 +0300

    [svn r253] Better support of QT texttrack

--- src/src/com/panayotis/jubler/subs/loader/text/Quicktime.java
+++ src/src/com/panayotis/jubler/subs/loader/text/Quicktime.java
@@ -2,7 +2,7 @@
  * SubRip.java
  *
  * Created on 26 Αύγουστος 2005, 11:08 πμ
- * 
+ *
  * This file is part of Jubler.
  *
  * Jubler is free software; you can redistribute it and/or modify
@@ -26,6 +26,7 @@ package com.panayotis.jubler.subs.loader.text;
 import com.panayotis.jubler.subs.loader.AbstractTextSubFormat;
 import com.panayotis.jubler.subs.SubEntry;
 import com.panayotis.jubler.time.Time;
+import com.sun.tools.doclets.internal.toolkit.util.Group;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,28 +43,21 @@ public class Quicktime extends AbstractTextSubFormat {
     
     private static final Pattern pat, test_pat;
     
-    private Time start, finish;
+    private Time start, finish, mediafinish;
+    String text;
     
     static {
         pat = Pattern.compile(
-                "\\[(\\d\\d):(\\d\\d):(\\d\\d)\\.(\\d+)\\]"+nl+"(.*?)"+nl
+                "(?s)\\[(\\d\\d):(\\d\\d):(\\d\\d)\\.(\\d+)\\](.*?)((?=\\[)|\\z)"
                 );
-        test_pat = Pattern.compile("^\\{QTtext\\}");
+        test_pat = Pattern.compile("\\A\\{QTtext\\}");
     }
     
-    protected Pattern getPattern () {
+    protected Pattern getPattern() {
         return pat;
     }
-     protected Pattern getTestPattern() {
+    protected Pattern getTestPattern() {
         return test_pat;
-    }
-    
-    
-    protected SubEntry getSubEntry(Matcher m) {
-        Time start = new Time(m.group(1), m.group(2), m.group(3), m.group(4));
-        String text = m.group(5).trim();
-        if (text.equals("")) return null;
-        return new SubEntry (start, start, m.group(5));
     }
     
     
@@ -74,26 +68,48 @@ public class Quicktime extends AbstractTextSubFormat {
     public String getName() {
         return "Quicktime";
     }
-     public String getExtendedName() {
+    public String getExtendedName() {
         return _("Quicktime Texttrack");
     }
     
     protected String initLoader(String input) {
+        start = null;
         return super.initLoader(input);
     }
-
+    protected SubEntry getSubEntry(Matcher m) {
+        /* Initialize matcher */
+        if (start==null) {
+            start = new Time(m.group(1), m.group(2), m.group(3), m.group(4));
+            text = m.group(5).replace('\n', ' ').trim();
+            if (text.equals("")) text = null;
+            return null;
+        }
+        /* Get current timing */
+        finish = new Time(m.group(1), m.group(2), m.group(3), m.group(4));
+        /* Prepare older subtitle */
+        SubEntry ret = null;
+        if (text!=null) ret = new SubEntry(start, finish, text);
+        /* Prepare current subtitle */
+        start = finish;
+        text = m.group(5).replace('\n', ' ').trim();
+        if (text.equals("")) text = null;
+        return ret;
+    }
     
     
     protected void initSaver(Subtitles subs, MediaFile media, StringBuffer header) {
-        header.append("{QTtext}\n");
+        header.append("{QTtext}{timeScale:1000}{timeStamps:absolute}\n");
         start = null;
         finish = null;
+        if (media.getVideoFile()!=null)
+            mediafinish = new Time(media.getVideoFile().getLength());
+        else mediafinish = new Time(0);
     }
     protected void appendSubEntry(SubEntry sub, StringBuffer str){
         if (start==null) {
             start = sub.getStartTime();
             if (start.toSeconds()>0d) {
-                str.append("[00:00:00.000]\n\n");
+                str.append("[00:00:00.000]\n");
             }
         }
         str.append('[');
@@ -103,8 +119,9 @@ public class Quicktime extends AbstractTextSubFormat {
         finish = sub.getFinishTime();
     }
     protected void cleanupSaver(StringBuffer footer) {
+        if (finish.compareTo(mediafinish)<0) finish = mediafinish;
         footer.append('[').append(finish.toString().replace(',', '.')).append("]\n");
     }
-
+    
     public boolean supportsFPS() { return false; }
 }
