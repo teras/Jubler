3e50f43ee92d8580ebe046910f964c3d96781153
teras <teras@localhost>
2008-11-16 17:11:45 +0200

    [svn r530] Get changelog file using build-in jupidator functions

--- src/src/com/panayotis/jubler/information/AutoUpdater.java
+++ src/src/com/panayotis/jubler/information/AutoUpdater.java
@@ -7,12 +7,16 @@ package com.panayotis.jubler.information;
 import com.panayotis.jubler.StaticJubler;
 import com.panayotis.jubler.os.DEBUG;
 import com.panayotis.jubler.os.SystemDependent;
+import com.panayotis.jubler.os.SystemFileFinder;
 import com.panayotis.jupidator.ApplicationInfo;
 import com.panayotis.jupidator.Updater;
 import com.panayotis.jupidator.UpdaterException;
 import com.panayotis.jupidator.UpdaterListener;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,7 +30,7 @@ public class AutoUpdater implements UpdaterListener {
             current.load(AutoUpdater.class.getResource("/com/panayotis/jubler/information/version.prop").openStream());
 
             ApplicationInfo ap = new ApplicationInfo(
-                    System.getProperty("user.home") + "/Works/Development/Java/Jubler/testcase/release",
+                    SystemFileFinder.getJublerAppPath(),
                     SystemDependent.getConfigPath(),
                     SystemDependent.getAppSupportDirPath(),
                     current.getProperty("release"),
@@ -35,7 +39,7 @@ public class AutoUpdater implements UpdaterListener {
 
             new Updater(
                     "file://" + System.getProperty("user.home") + "/Works/Development/Java/Jubler/resources/system/updater.xml",
-                    ap, this);
+                    ap, this).actionStart();
 
         } catch (IOException ex) {
             DEBUG.debug(ex);
@@ -47,8 +51,39 @@ public class AutoUpdater implements UpdaterListener {
     public boolean requestRestart() {
         return StaticJubler.requestQuit(null);
     }
-    
+
     public void receiveMessage(String message) {
         DEBUG.debug(message);
+    }
+
+    /**
+     * Use this method to save changelog to a file
+     * @param args
+     */
+    public static void main(String[] args) {
+        {
+            FileWriter out = null;
+            try {
+                if (args.length < 1) {
+                    System.err.println("Arguments should be greater than 0.");
+                    return;
+                }
+                String cl = new Updater("file://" + System.getProperty("user.home") + "/Works/Development/Java/Jubler/resources/system/updater.xml", null, null).getChangeLog();
+                out = new FileWriter(args[0]);
+                out.write(cl);
+                out.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (UpdaterException ex) {
+                ex.printStackTrace();
+            } finally {
+                try {
+                    if (out != null)
+                        out.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
