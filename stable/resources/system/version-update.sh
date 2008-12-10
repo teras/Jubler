a9a83dd2d5d29a56450c59bfcb31b4c209e39700
teras <teras@localhost>
2008-12-10 12:10:20 +0200

    [svn r591] Improved system for updating version number

--- stable/resources/system/version-update.sh
+++ stable/resources/system/version-update.sh
@@ -2,12 +2,16 @@
 
 FPATH=`dirname $0`/version.prop
 
+RELEASE=`svn info | grep Revision | awk '{print $2'}`
+
 echo -n "Please give human-readable version: "
 read HVER
 echo -n "Please give numeric version: "
 read NVER
 echo
-echo "Human readable version is \"$HVER\" and numeric version is \"$NVER\"."
+echo "Human readable version: $HVER"
+echo "Numeric version: $NVER"
+echo "Release: $RELEASE"
 echo "Press [RETURN] to continue or [CTRL]-C to abort version upgrade."
 read NL
 echo
@@ -15,11 +19,12 @@ echo Updating file $FPATH
 cat >$FPATH <<EOF
 version=$HVER
 longversion=$NVER
-release=@RELEASE@
+release=${RELEASE}
 packaged=@DISTRIBUTION@
 EOF
 cd `dirname $0`
 cd ../..
-ant version-update
+
+ant nodistbased
 echo
 echo "Version update completed"
