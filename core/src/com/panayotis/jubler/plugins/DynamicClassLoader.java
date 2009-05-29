ab12813a8fc07ed7447ef4f1f596b3db3a03717c
Panayotis Katsaloulis <Panayotis Katsaloulis@localhost>
2009-05-29 16:22:41 +0300

    Added dynamic plugin definition inside jar files

--- core/src/com/panayotis/jubler/plugins/DynamicClassLoader.java
+++ core/src/com/panayotis/jubler/plugins/DynamicClassLoader.java
@@ -21,12 +21,19 @@
  */
 package com.panayotis.jubler.plugins;
 
+import com.panayotis.jubler.os.DEBUG;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  *
@@ -36,9 +43,14 @@ public class DynamicClassLoader extends URLClassLoader {
 
     private final static String FS = System.getProperty("file.separator");
     private final static String UD = System.getProperty("user.dir") + FS;
-
+    private final static String plugins_list_filename = "plugins.list";
     public static String MainPath = UD;
     private static boolean MainPath_JarBased = false;
+    private static final ArrayList<String> plugins_list = new ArrayList<String>();
+
+    public static ArrayList<String> getPluginsList() {
+        return plugins_list;
+    }
 
     public DynamicClassLoader(String[] paths) {
         this(paths, true);
@@ -73,8 +85,36 @@ public class DynamicClassLoader extends URLClassLoader {
         if (path.exists() && path.getPath().toLowerCase().endsWith(".jar"))
             try {
                 urls.add(path.toURL());
+                addPluginsList(path);
             } catch (MalformedURLException ex) {
             }
+    }
+
+    private static void addPluginsList(File path) {
+        BufferedReader in = null;
+        try {
+            InputStream stream = new ZipFile(path).getInputStream(new ZipEntry(plugins_list_filename));
+            if (stream != null) {
+                in = new BufferedReader(new InputStreamReader(stream));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    line = line.trim();
+                    if (line.length() > 0) {
+                        DEBUG.debug("Registering plugin " + line + " from file" + path.getPath() + ".");
+                        plugins_list.add(line);
+                    }
+                }
+                in.close();
+                return;
+            }
+        } catch (IOException ex) {
+        } finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException ex) {
+            }
+        }
     }
 
     public static void updateMainPath(String basename, String baseclass) {
@@ -90,7 +130,7 @@ public class DynamicClassLoader extends URLClassLoader {
                 MainPath_JarBased = true;
                 return;
             }
-            if (new File(path + FS + baseclass.replace('.', FS.charAt(0))+".class").exists()) {
+            if (new File(path + FS + baseclass.replace('.', FS.charAt(0)) + ".class").exists()) {
                 MainPath = path + FS;
                 MainPath_JarBased = false;
                 return;
