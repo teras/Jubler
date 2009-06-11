b973820433ea6318c0579d9e4e289c8ea72fb49e
hoangduytran1960 <hoangduytran1960@googlemail.com>
2009-06-11 21:30:19 +0100

    Made use of the static routine to obtain the selected component, reduce the amount of duplicated code.

--- src/com/panayotis/jubler/tools/editing/EditCopy.java
+++ src/com/panayotis/jubler/tools/editing/EditCopy.java
@@ -66,20 +66,12 @@ public class EditCopy extends JMenuItem implements ActionListener {
 
     public void actionPerformed(java.awt.event.ActionEvent evt) {
         try {
-            
-            if (compSel == null) {
-                compSel = new ComponentSelection(jublerParent);
-            } else {
-                compSel.setJubler(jublerParent);
-            }
-            SubtitleRecordComponent opt = compSel.showDialog();
-            boolean is_valid_opt = (opt != SubtitleRecordComponent.CP_INVALID);
-            if (!is_valid_opt) {
+            SubtitleRecordComponent opt =
+                    ComponentSelection.getSelectedComponent(jublerParent, true);
+            if (opt == null) {
                 return;
             }
             
-            Jubler.selectedComponent = opt;
-
             SubEntry sub = null;
             int row = -1;
 
