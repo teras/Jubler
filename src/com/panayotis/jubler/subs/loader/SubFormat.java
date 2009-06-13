54e5a50866c028a2d0545b91ead3bffb5dc51dcf
hoangduytran1960 <hoangduytran1960@googlemail.com>
2009-06-13 07:11:46 +0100

    Optimised the running of the 'getSubImageDimension', reducing the number of loops to a half that was required before. Introduced the 'compressImage' to attempt to reduce the required memory size, however, as tests shown the use of this routine does not eliminate the need for setting '-Xms128m -Xmx102m' in the command-line when running, the code is kept for reference and future use but not being used at the moment. The old 'technique' of using '-Xms128m -Xmx102m' when working with images is still required.

--- src/com/panayotis/jubler/subs/loader/SubFormat.java
+++ src/com/panayotis/jubler/subs/loader/SubFormat.java
@@ -22,6 +22,7 @@
  */
 package com.panayotis.jubler.subs.loader;
 
+import static com.panayotis.jubler.i18n.I18N._;
 import com.panayotis.jubler.media.MediaFile;
 import com.panayotis.jubler.options.JPreferences;
 import com.panayotis.jubler.subs.Share;
@@ -29,9 +30,17 @@ import com.panayotis.jubler.subs.Subtitles;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import javax.swing.ImageIcon;
 
 /**
@@ -152,6 +161,7 @@ public abstract class SubFormat {
                         sr.width,
                         sr.height);
 
+                //BufferedImage comp_image = compressImage(sub_img, 0.1f);
                 /**
                  * Converts the cropped image into the image-icon for displaying
                  */
@@ -205,8 +215,8 @@ public abstract class SubFormat {
             Color find_colour,
             BufferedImage dest) {
         Rectangle rec = null;
-        int lx,  ly,  rx,  ry;
-        int img_w,  img_h,  w,  h;
+        int lx, ly, rx, ry;
+        int img_w, img_h, w, h;
         try {
             img_w = source.getWidth();
             img_h = source.getHeight();
@@ -216,6 +226,11 @@ public abstract class SubFormat {
              */
             lx = img_w;
             ly = img_h;
+            /**
+             * Assuming the smallest value first.
+             */
+            rx = 0;
+            ry = 0;
 
             /**
              * search top-left down to bottom-right and find the excluding 
@@ -233,36 +248,19 @@ public abstract class SubFormat {
                         if (y < ly) {
                             ly = y;
                         }
-                        /*copy the non-transparent pixels to new image*/
-                        dest.setRGB(x, y, img_rgb);
-                    }//end if
-                }//end for (int col = 0; col < img_h; col++)
-            }//end for(int row = 0; row < img_w; row++)
-
-            /**
-             * Assuming the smallest value first.
-             */
-            rx = 0;
-            ry = 0;
-            /**
-             * search bottom-right up to top-left, upto the limit above
-             * and find the excluding colour. The larger value is held.
-             */
-            for (int x = img_w - 1; x > lx; x--) {
-                for (int y = img_h - 1; y > ly; y--) {
-                    int img_rgb = (source.getRGB(x, y));
-                    Color img_colour = new Color(img_rgb);
-                    boolean is_same = (find_colour.equals(img_colour));
-                    if (!is_same) {
+                        
                         if (x > rx) {
                             rx = x;
                         }
                         if (y > ry) {
                             ry = y;
                         }
+                        
+                        /*copy the non-transparent pixels to new image*/
+                        dest.setRGB(x, y, img_rgb);
                     }//end if
                 }//end for (int col = 0; col < img_h; col++)
-            }//end for(int row = 0; row < img_w; row++)            
+            }//end for(int row = 0; row < img_w; row++)
 
             /**
              * Calculate the width and height
@@ -286,7 +284,7 @@ public abstract class SubFormat {
      */
     public static BufferedImage getTransparentImage(BufferedImage source, Color find_colour) {
         BufferedImage tran_image = null;
-        int img_w,  img_h,  w,  h;
+        int img_w, img_h, w, h;
         try {
             w = source.getWidth();
             h = source.getHeight();
@@ -326,8 +324,8 @@ public abstract class SubFormat {
      */
     public static Rectangle getSubImageDimension(BufferedImage img, Color find_colour) {
         Rectangle rec = null;
-        int lx,  ly,  rx,  ry;
-        int img_w,  img_h,  w,  h;
+        int lx, ly, rx, ry;
+        int img_w, img_h, w, h;
         try {
             img_w = img.getWidth();
             img_h = img.getHeight();
@@ -337,6 +335,11 @@ public abstract class SubFormat {
              */
             lx = img_w;
             ly = img_h;
+            /**
+             * Assuming the smallest value first.
+             */
+            rx = 0;
+            ry = 0;
 
             /**
              * search top-left down to bottom-right and find the excluding 
@@ -354,25 +357,6 @@ public abstract class SubFormat {
                         if (y < ly) {
                             ly = y;
                         }
-                    }//end if
-                }//end for (int col = 0; col < img_h; col++)
-            }//end for(int row = 0; row < img_w; row++)
-
-            /**
-             * Assuming the smallest value first.
-             */
-            rx = 0;
-            ry = 0;
-            /**
-             * search bottom-right up to top-left, upto the limit above
-             * and find the excluding colour. The larger value is held.
-             */
-            for (int x = img_w - 1; x > lx; x--) {
-                for (int y = img_h - 1; y > ly; y--) {
-                    int img_rgb = (img.getRGB(x, y));
-                    Color img_colour = new Color(img_rgb);
-                    boolean is_same = (find_colour.equals(img_colour));
-                    if (!is_same) {
                         if (x > rx) {
                             rx = x;
                         }
@@ -381,7 +365,7 @@ public abstract class SubFormat {
                         }
                     }//end if
                 }//end for (int col = 0; col < img_h; col++)
-            }//end for(int row = 0; row < img_w; row++)            
+            }//end for(int row = 0; row < img_w; row++)
 
             /**
              * Calculate the width and height
@@ -393,4 +377,38 @@ public abstract class SubFormat {
         }
         return rec;
     }//end public Rectangle getSubImageDimension(BufferedImage img, Color col)
+
+    public static BufferedImage compressImage(BufferedImage image, float quality) throws IOException {
+        //int orig_size = image.getData().getDataBuffer().getSize();
+        
+        //System.out.println("Original size:" + orig_size);
+        
+        // Get a ImageWriter for jpeg format.
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersBySuffix("jpeg");
+        if (!writers.hasNext()) {
+            throw new IllegalStateException(_("No writers found"));
+        }
+        ImageWriter writer = (ImageWriter) writers.next();
+        // Create the ImageWriteParam to compress the image.
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(quality);
+        // The output will be a ByteArrayOutputStream (in memory)
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(32768);
+        ImageOutputStream ios = ImageIO.createImageOutputStream(bos);
+        writer.setOutput(ios);
+        writer.write(null, new IIOImage(image, null, null), param);
+        ios.flush(); // otherwise the buffer size will be zero!
+        
+        // From the ByteArrayOutputStream create a RenderedImage.
+        ByteArrayInputStream in = new ByteArrayInputStream(bos.toByteArray());
+        BufferedImage out = ImageIO.read(in);        
+        //int compressed_size = out.getData().getDataBuffer().getSize();
+        //System.out.println("Compressed size:" + compressed_size);
+        return out;
+    // Uncomment code below to save the compressed files.
+    //    File file = new File("compressed."+quality+".jpeg");
+    //    FileImageOutputStream output = new FileImageOutputStream(file);
+    //    writer.setOutput(output); writer.write(null, new IIOImage(image, null,null), param);
+    }
 }
