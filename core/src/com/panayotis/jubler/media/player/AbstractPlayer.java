b11945ad566a10fd771f6569799e1665ebfc1b9b
Panayotis Katsaloulis <Panayotis Katsaloulis@localhost>
2009-07-01 13:31:07 +0300

    format fixing of abstractplayer

--- core/src/com/panayotis/jubler/media/player/AbstractPlayer.java
+++ core/src/com/panayotis/jubler/media/player/AbstractPlayer.java
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-
 package com.panayotis.jubler.media.player;
 
 import com.panayotis.jubler.os.DEBUG;
@@ -37,25 +36,27 @@ import com.panayotis.jubler.os.Networking;
 import com.panayotis.jubler.subs.SubFile;
 import com.panayotis.jubler.time.Time;
 import java.util.StringTokenizer;
+
 /**
  *
  * @author teras
  */
 public abstract class AbstractPlayer extends VideoPlayer {
-    
+
     private AbstractPlayerOptions opts;
     private String subpath;
     private int x, y;
-    
-    
+
     public AbstractPlayer(String family) {
         opts = new AbstractPlayerOptions(family, this);
     }
-    
+
     public abstract String getDefaultArguments();
+
     public abstract String[] getTestParameters();
-    public abstract String getTestSignature();    
-    
+
+    public abstract String getTestSignature();
+
     /* Create Subtitle File for testing*/
     private void initSubFile(Subtitles subs, MediaFile mfile) {
         try {
@@ -66,54 +67,54 @@ public abstract class AbstractPlayer extends VideoPlayer {
         } catch (IOException ex) {
         }
     }
-    
-     public void cleanUp() {
-        if (subpath==null) return;
+
+    public void cleanUp() {
+        if (subpath == null)
+            return;
         File f = new File(subpath);
-        if (f.exists()) f.delete();
+        if (f.exists())
+            f.delete();
     }
-    
-    private void replaceValues(String[]args, String pattern, String value) {
+
+    private void replaceValues(String[] args, String pattern, String value) {
         int pos;
-        for (int i = 0 ; i < args.length ; i++) {
+        for (int i = 0; i < args.length; i++) {
             pos = args[i].indexOf(pattern);
             if (pos >= 0) {
-                args[i] = args[i].substring(0,pos) + value + args[i].substring(pos+pattern.length());
+                args[i] = args[i].substring(0, pos) + value + args[i].substring(pos + pattern.length());
                 return;
             }
         }
     }
-    
-    public String[] getCommandArguments( MediaFile mfile, Subtitles sub, Time when ) {
-        
+
+    public String[] getCommandArguments(MediaFile mfile, Subtitles sub, Time when) {
+
         /* Frist, find out if we need different audio stream */
         String options = opts.getArguments();
         int begin, end;
         begin = options.indexOf("%(");
         end = options.lastIndexOf("%)");
-        if (begin >= 0 && end < options.length() && begin < end) {
-            if ( mfile.getAudioFile().isSameAsVideo()) {
-                options = options.substring(0,begin) + options.substring(end+2,options.length());
-            } else {
-                options = options.substring(0,begin) + options.substring(begin+2,end) + options.substring(end+2,options.length());
-            }
-        }
-        options = options.replaceAll("%\\(","");
-        options = options.replaceAll("%\\)","");
-        
+        if (begin >= 0 && end < options.length() && begin < end)
+            if (mfile.getAudioFile().isSameAsVideo())
+                options = options.substring(0, begin) + options.substring(end + 2, options.length());
+            else
+                options = options.substring(0, begin) + options.substring(begin + 2, end) + options.substring(end + 2, options.length());
+        options = options.replaceAll("%\\(", "");
+        options = options.replaceAll("%\\)", "");
+
         /* tokenize command line */
         StringTokenizer st = new StringTokenizer(options, " ");
         String[] cmds = new String[st.countTokens()];
         initSubFile(sub, mfile);
         String buf;
         int pos = 0;
-        
-        for (int i = 0 ; i < cmds.length ; i++) {
+
+        for (int i = 0; i < cmds.length; i++) {
             buf = st.nextToken();
-            if (buf!=null && (!buf.equals("")) )
+            if (buf != null && (!buf.equals("")))
                 cmds[pos++] = buf;
         }
-        
+
         replaceValues(cmds, "%p", opts.getExecFileName());
         replaceValues(cmds, "%v", mfile.getVideoFile().getPath());
         replaceValues(cmds, "%a", mfile.getAudioFile().getPath());
@@ -122,23 +123,29 @@ public abstract class AbstractPlayer extends VideoPlayer {
         replaceValues(cmds, "%x", Integer.toString(x));
         replaceValues(cmds, "%y", Integer.toString(y));
         replaceValues(cmds, "%j", SystemFileFinder.getJublerAppPath());
-        replaceValues(cmds, "%i",Integer.toOctalString(Networking.getRandomPort()));
-        
+        replaceValues(cmds, "%i", Integer.toOctalString(Networking.getRandomPort()));
+
         StringBuffer cm = new StringBuffer();
-        for (int i = 0 ; i < cmds.length ; i++) {
+        for (int i = 0; i < cmds.length; i++)
             cm.append(cmds[i]).append(' ');
-        }
         DEBUG.debug(cm.toString());
         return cmds;
     }
-    
-    public JExtBasicOptions getOptionsPanel() { return opts; }
-    
+
+    public JExtBasicOptions getOptionsPanel() {
+        return opts;
+    }
+
     public void setCentralLocation(int x, int y) {
-        this.x = x ;
-        this.y = y ;
-}
-    
-    public int getLocationX() { return x; }
-    public int getLocationY() { return y; }
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getLocationX() {
+        return x;
+    }
+
+    public int getLocationY() {
+        return y;
+    }
 }
