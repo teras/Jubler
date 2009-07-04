327dc07945f508067ecd44515253d22c07e8e676
Panayotis Katsaloulis <Panayotis Katsaloulis@localhost>
2009-07-04 20:08:17 +0300

    find random port, in networking class

--- core/src/com/panayotis/jubler/os/Networking.java
+++ core/src/com/panayotis/jubler/os/Networking.java
@@ -5,13 +5,31 @@
 
 package com.panayotis.jubler.os;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  *
  * @author teras
  */
 public class Networking {
 
-    public static int getRandomPort() {
-        return 55600;
+        public static int getRandomPort() {
+        ServerSocket soc;
+        for (int i = 50000; i < 51000; i++) {
+            soc = null;
+            try {
+                soc = new ServerSocket(i);
+            } catch (Exception ex) {
+            }
+            if (soc != null) {
+                try {
+                    soc.close();
+                } catch (IOException ex) {
+                }
+                return i;
+            }
+        }
+        return -1;
     }
 }
