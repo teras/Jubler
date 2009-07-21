7827277d0a28f1707dadecef397b9a5b5d78f301
hoang_tran <hoang_tran@localhost>
2009-07-21 21:35:40 +0100

    Fixed comments.

--- src/com/panayotis/jubler/subs/loader/binary/package-info.java
+++ src/com/panayotis/jubler/subs/loader/binary/package-info.java
@@ -30,21 +30,21 @@
  * This package contains classes that are used to load binary or sub-binary 
  * format files. An instance of such is the DVDMaestro's SON format files.
  * Although the subtitle index file holds a pure textual content, the 
- * attachement of the images qualifies it to be a binary format. The parsing
+ * attachement of images qualifies it to be a binary format. The parsing
  * mechanism for classes in this package is more complex than the one
  * presented in the 
  * {@link com.panayotis.jubler.subs.loader.AbstractTextSubFormat}
- * and it is done so to adapt to the complex format of data, such as files
- * with different blocks and each blocks contains values with different 
- * meaning. <br><br>
+ * and it is done so to adapt to the complex format of data the subtitle files
+ * inherited. Example of such instance are files with different blocks and 
+ * each blocks contains values with different meaning. <br><br>
  * The parsing mechanism represented in the 
  * {@link com.panayotis.jubler.subs.loader.AbstractBinarySubFormat}
  * is used with a list of 
  * {@link com.panayotis.jubler.subs.SubtitlePatternProcessor}, 
  * each in charge of processing one pattern.
- * The parsing mechanism also generates several events in which the instances
+ * The parsing mechanism also generates several events in which instances
  * of {@link com.panayotis.jubler.subs.loader.AbstractTextSubFormat}
- * can catch and transform and manipulate data.
+ * can catch to transform, manipulate data or alter the state of processing.
  */
 package com.panayotis.jubler.subs.loader.binary;
 
