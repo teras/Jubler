ca4336ae770094b279330ee4be14912c669651d9
hoang_tran <hoang_tran@localhost>
2009-10-20 20:01:52 +0100

    Simplified version, removed the extension from JMenuItem, allowing the GUI to be managed and created by IDE.

--- src/com/panayotis/jubler/MenuAction.java
+++ src/com/panayotis/jubler/MenuAction.java
@@ -27,56 +27,25 @@
  */
 package com.panayotis.jubler;
 
-import com.panayotis.jubler.subs.Share;
 import java.awt.event.ActionListener;
-import javax.swing.JMenuItem;
 
 /**
  *
  * @author hoang_tran <hoangduytran1960@googlemail.com>
  */
-public abstract class MenuAction extends JMenuItem implements ActionListener {
+public abstract class MenuAction implements ActionListener {
 
-    protected String actionName = null;
     protected Jubler jublerParent = null;
 
     public MenuAction() {}
     
     /**
-     * Default constructor, setting action-name and add the action listener
-     * implemented by this class.
-     */
-    public MenuAction(String actionName) {
-        this.actionName = actionName;
-        if (! Share.isEmpty(actionName)) {
-            setText(actionName);
-            setName(actionName);
-        }//end if
-        addActionListener(this);
-    }
-
-    /**
      * Peform default construction with the addition of setting reference
      * for the {@link Jubler} parent.
      * @param jublerParent The reference to {@link Jubler} running instance.
      */
-    public MenuAction(Jubler jublerParent, String actionName) {
-        this(actionName);
+    public MenuAction(Jubler jublerParent) {
         this.jublerParent = jublerParent;
-    }
-
-    /**
-     * @return the actionName
-     */
-    public String getActionName() {
-        return actionName;
-    }
-
-    /**
-     * @param actionName the actionName to set
-     */
-    public void setActionName(String actionName) {
-        this.actionName = actionName;
     }
 
     /**
