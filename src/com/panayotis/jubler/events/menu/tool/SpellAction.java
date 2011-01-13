5339933463367e459ac2ed2fee8adb3c1889737f
hdq <hdq@linux-8df1>
2011-01-13 22:38:34 +0000

    Changed the behaviour of the spelling action to mimic the effect of spell checking from document, that the spelling action, by default, start from the currently selected row to the end of the document. This allow suspension of the spell-checking process to perform corrections, which spell checker cannot perform, then continue from where left-off.

--- src/com/panayotis/jubler/events/menu/tool/SpellAction.java
+++ src/com/panayotis/jubler/events/menu/tool/SpellAction.java
@@ -25,13 +25,17 @@
  * Contributor(s):
  * 
  */
-
 package com.panayotis.jubler.events.menu.tool;
 
 import com.panayotis.jubler.Jubler;
 import com.panayotis.jubler.MenuAction;
-import com.panayotis.jubler.tools.JSpeller;
+import com.panayotis.jubler.events.menu.edit.undo.UndoEntry;
+import com.panayotis.jubler.events.menu.tool.spell.JSpellChecker;
+import com.panayotis.jubler.subs.SubEntry;
+import com.panayotis.jubler.subs.Subtitles;
 import java.awt.event.ActionEvent;
+import java.util.Vector;
+import javax.swing.JTable;
 
 /**
  *
@@ -48,7 +52,61 @@ public class SpellAction extends MenuAction {
      * @param e Action Event
      */
     public void actionPerformed(ActionEvent evt) {
-        JSpeller spell = new JSpeller();
-        spell.execute(jublerParent);
+        Vector<SubEntry> affected_list = null;
+        Jubler jb = jublerParent;
+        Subtitles subs = jb.getSubtitles();
+        JTable subTable = jublerParent.getSubTable();
+        int select_count = subTable.getSelectedRowCount();
+        boolean use_selection = (select_count > 1);
+        if (use_selection) {
+            affected_list = getSelectionList(subTable, subs);
+        } else {
+            affected_list = getListFromCurrentToEnd(subTable, subs);
+        }
+
+        jb.getUndoList().addUndo(new UndoEntry(subs, "Spell Checker"));
+        JSpellChecker checkvisual = new JSpellChecker(jublerParent, Jubler.prefs.getSpellChecker(), affected_list);
+        checkvisual.findNextWord();
     }//end public void actionPerformed(ActionEvent evt)
+
+    /**
+     * The selection list of subtitle entries. This selection list will hold
+     * two or more items as user selected by holding down shift keys.
+     * @param subTable The subtitle table, GUI component.
+     * @param subList The data list of subtitle entries.
+     * @return The vector list of selected entries.
+     */
+    private Vector<SubEntry> getSelectionList(JTable subTable, Subtitles subList) {
+        Vector<SubEntry> list = new Vector<SubEntry>();
+        try {
+            int[] selected_indices = subTable.getSelectedRows();
+            for (int i = 0; i < selected_indices.length; i++) {
+                SubEntry entry = subList.elementAt(i);
+                list.add(entry);
+            }//end for(int i=0; i < selected_indices.length; i++)
+        } catch (Exception ex) {
+        }//end try/catch
+        return list;
+    }//end private Vector<SubEntry> getSelectionList(JTable subTable)
+
+    /**
+     * Getting the affected list from the current selected row to the end of
+     * the list.
+     * @param subTable The subtitle table, GUI component.
+     * @param subList The data list of subtitle entries.
+     * @return The vector list of selected entries.
+     */
+    private Vector<SubEntry> getListFromCurrentToEnd(JTable subTable, Subtitles subList) {
+        Vector<SubEntry> list = new Vector<SubEntry>();
+        try {
+            int selected_row = subTable.getSelectedRow();
+            int length = subList.size();
+            for (int i = selected_row; i < length; i++) {
+                SubEntry entry = subList.elementAt(i);
+                list.add(entry);
+            }//end for(int i=0; i < selected_indices.length; i++)
+        } catch (Exception ex) {
+        }//end try/catch
+        return list;
+    }//end private Vector<SubEntry> getSelectionList(JTable subTable)
 }//end public class SpellAction extends MenuAction
