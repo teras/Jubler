7326f7290b2030cda7af70e3f18638fd5e0348a1
Hoang Tran <Hoang Tran@homepc>
2011-06-20 09:21:39 +0100

    Added BinarySubs plugin to handle formats: SON, SWT, TMPGenc, SUP

---
+++ plugins/binarysubs/src/com/panayotis/jubler/subs/loader/binary/SWT/SWTPatternDef.java
@@ -0,0 +1,52 @@
+/*
+ * SWTPatternDef.java
+ *
+ * Created on 12-Dec-2008 by Hoang Duy Tran <hoang_tran>
+ */
+
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * This file is part of Jubler.
+ * 
+ * Jubler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ * 
+ * 
+ * Jubler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Jubler; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Contributor(s):
+ * 
+ */
+package com.panayotis.jubler.subs.loader.binary.SWT;
+
+import com.panayotis.jubler.subs.CommonDef;
+
+/**
+ * Common definitions of patterns for processing of SWT (Son With Text) subtitle
+ * files. This format is derived from the SON format by Hoang Duy Tran.
+ * @author Hoang Duy Tran <hoang_tran>
+ */
+public interface SWTPatternDef extends CommonDef {
+
+    /**
+     * "SP_NUMBER" + "\t" + "START" + "\t" + "END" + "\t" + "FILE_NAME" + "\t" + "SUBTITLE_TEXT"
+     */
+    public static String swtSubtitleEventHeaderLine = "SP_NUMBER" + "\t" + "START" + "\t" + "END" + "\t" + "FILE_NAME" + "\t" + "SUBTITLE_TEXT";
+    /**
+     * "(?i)SP_NUMBER" + sp + "START" + sp + "END" + sp + "FILE_NAME" + sp + "SUBTITLE_TEXT"
+     */
+    public static String p_swt_subtitle_event_header = "(?i)SP_NUMBER" + sp + "START" + sp + "END" + sp + "FILE_NAME" + sp + "SUBTITLE_TEXT";
+    /**
+     * printable
+     */
+    public static String p_swt_text = printable;
+}
---
+++ plugins/binarysubs/src/com/panayotis/jubler/subs/loader/binary/SWT/processor/SWTSubtitleText.java
@@ -0,0 +1,77 @@
+/*
+ * SWTSubtitleText.java
+ *
+ * Created on 11-Dec-2008 by Hoang Duy Tran <hoang_tran>
+ */
+
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * This file is part of Jubler.
+ * 
+ * Jubler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ * 
+ * 
+ * Jubler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Jubler; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Contributor(s):
+ * 
+ */
+package com.panayotis.jubler.subs.loader.binary.SWT.processor;
+
+import com.panayotis.jubler.subs.loader.binary.SWT.SWTPatternDef;
+import com.panayotis.jubler.subs.Share;
+import com.panayotis.jubler.subs.SubtitlePatternProcessor;
+import com.panayotis.jubler.subs.loader.binary.SWT.record.SWTSubEntry;
+
+
+/**
+ * This class is used to parse the subtitle text within the SWT subtilte file.
+ * @author Hoang Duy Tran <hoang_tran>
+ */
+public class SWTSubtitleText extends SubtitlePatternProcessor implements SWTPatternDef {
+
+    private static String pattern = anything;
+    private SWTSubEntry swtSubEntry = null;
+
+    public SWTSubtitleText() {
+        super(pattern);
+    }
+
+    public void parsePattern(String[] matched_data, Object record) {
+        String new_text = getTextLine();
+        if (record instanceof SWTSubEntry) {
+            setSwtSubEntry((SWTSubEntry) record);
+
+            String txt = getSwtSubEntry().getText();
+            boolean has_text = !Share.isEmpty(txt);
+            if (has_text) {
+                StringBuffer b = new StringBuffer();
+                b.append(getSwtSubEntry().getText());
+                b.append(UNIX_NL);
+                b.append(new_text);
+                getSwtSubEntry().setText(b.toString());
+            } else {
+                getSwtSubEntry().setText(new_text);
+            }//end if            
+        }//end if (record instanceof SonSubEntry)
+
+    }//end if
+
+    public SWTSubEntry getSwtSubEntry() {
+        return swtSubEntry;
+    }
+
+    public void setSwtSubEntry(SWTSubEntry swtSubEntry) {
+        this.swtSubEntry = swtSubEntry;
+    }
+}
