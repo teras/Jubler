9c9dbc6b53045cda223272594161acd06eb54739
Hoang Tran <Hoang Tran@homepc>
2011-06-20 09:16:57 +0100

    Set the reference of the JubFrame (currentWindow) for the format handler when they are being used to save the subtitle list.

--- core/src/com/panayotis/jubler/os/FileCommunicator.java
+++ core/src/com/panayotis/jubler/os/FileCommunicator.java
@@ -22,6 +22,7 @@
  */
 package com.panayotis.jubler.os;
 
+import com.panayotis.jubler.JubFrame;
 import static com.panayotis.jubler.i18n.I18N._;
 
 import java.io.BufferedReader;
@@ -108,7 +109,7 @@ public class FileCommunicator {
                     || (outfile.exists() && (!SystemDependent.canWrite(outfile))))
                 return _("File {0} is unwritable", outfile.getPath());
             sfile.getFormat().updateFormat(sfile);   // This is required to update FPS & encoding of the current format
-            sfile.getFormat().setJubler(subs.getJubler());
+            sfile.getFormat().setJubler(JubFrame.currentWindow);
             if (sfile.getFormat().produce(subs, tempout, media)) {  // produce & check if should rename file
                 outfile.delete();
                 if (!tempout.renameTo(outfile))
