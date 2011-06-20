13b95f5482b0eb1801878b7bd248b9911d2234aa
Hoang Tran <Hoang Tran@homepc>
2011-06-20 09:15:06 +0100

    Made the PlainText a single instance due to the fact that instances of format handler are now created anew when they are being  used. Also added the SubFormatOrderComparator and create a single instance of it when adding instances of SubFormat. Sort the list when items are added.

--- core/src/com/panayotis/jubler/subs/loader/AvailSubFormats.java
+++ core/src/com/panayotis/jubler/subs/loader/AvailSubFormats.java
@@ -24,6 +24,8 @@ package com.panayotis.jubler.subs.loader;
 
 import com.panayotis.jubler.plugins.PluginManager;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  *
@@ -32,6 +34,8 @@ import java.util.ArrayList;
 public class AvailSubFormats {
 
     private final ArrayList<SubFormat> Formats;
+    private PlainText pl_txt = new PlainText();
+    private SubFormatOrderComparator sfmt_order_comp = new SubFormatOrderComparator();
     int current;
 
     /** Creates a new instance of SubFormats */
@@ -39,12 +43,14 @@ public class AvailSubFormats {
         current = 0;
         Formats = new ArrayList<SubFormat>();
         PluginManager.manager.callPluginListeners(this);
-        add(new PlainText());
+        pl_txt.setClassLoader(ClassLoader.getSystemClassLoader());
+        add(pl_txt);
     }
 
     public boolean hasMoreElements() {
-        if (current < Formats.size())
+        if (current < Formats.size()) {
             return true;
+        }
         return false;
     }
 
@@ -57,28 +63,98 @@ public class AvailSubFormats {
     }
 
     public SubFormat findFromDescription(String name) {
-        if (name == null)
+        if (name == null) {
             return null;
-        for (int i = 0; i < Formats.size(); i++)
-            if (Formats.get(i).getDescription().equals(name))
-                return Formats.get(i);
+        }
+        for (int i = 0; i < Formats.size(); i++) {
+            SubFormat fmt =  Formats.get(i);
+            String desc = fmt.getDescription();
+            boolean is_found = desc.equals(name);
+            if (is_found) {
+                return fmt;
+            }
+        }
         return null;
     }
 
     public SubFormat findFromName(String ext) {
-        if (ext == null)
+        if (ext == null) {
             return null;
-        for (int i = 0; i < Formats.size(); i++)
-            if (Formats.get(i).getName().equals(ext))
+        }
+        for (int i = 0; i < Formats.size(); i++) {
+            if (Formats.get(i).getName().equals(ext)) {
                 return Formats.get(i);
+            }
+        }
         return null;
     }
+
+    /**
+     * Find a single instance of handler that handle the given extension.
+     * Since the same file extension can be used by multiple formats,
+     * the routine will return null when more than one instances are found.
+     * @param ext The subtitle file's extension given.
+     * @return unique file handler for the given extension, null if not found
+     * or there are more than one handler which can handle the same file 
+     * extension.
+     */
+    public SubFormat findFromExtension(String ext) {
+        boolean is_found = false;
+        String found_extension = null;
+        SubFormat format = null;
+        ArrayList<SubFormat> found_list = new ArrayList<SubFormat>();
+        for (SubFormat found_format : Formats) {
+            found_extension = found_format.getExtension();
+            is_found = found_extension.equalsIgnoreCase(ext);
+            if (is_found) {
+                found_list.add(found_format);
+            }//end if
+        }//end for (SubFormat found_format : Formats)
+        is_found = (found_list.size() == 1);
+        if (is_found) {
+            format = found_list.get(0);
+        }//end if (is_found)
+        return format;
+    }//end public SubFormat findFromExtension(String ext) 
 
     public SubFormat get(int i) {
         return Formats.get(i);
     }
 
+    /**
+     * Add one instance of the format into the list and sort
+     * the instances into the order number.
+     * @param format The instance of format to be added.
+     */
     public void add(SubFormat format) {
         Formats.add(format);
+        Collections.sort(Formats, sfmt_order_comp);
+    }//end public void add(SubFormat format)
+
+    /**
+     * @return the Formats
+     */
+    public ArrayList<SubFormat> getFormats() {
+        return Formats;
     }
+}
+
+class SubFormatOrderComparator implements Comparator<SubFormat>{
+
+    public int compare(SubFormat o1, SubFormat o2) {
+        int comp = -1;
+        try {
+            boolean is_same = (o1 == o2);
+            if (is_same) {
+                comp = 0;
+            } else {
+                int o1_order = o1.getFormatOrder();
+                int o2_order = o2.getFormatOrder();
+                comp = o1_order - o2_order;
+            }//end if (is_same)/else
+        } catch (Exception ex) {
+        }
+        return comp;
+    }//end public int compare(SubFormat o1, SubFormat o2) 
+    
 }
