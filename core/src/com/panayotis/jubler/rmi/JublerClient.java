40c362e9005b4ef407973b4eb2312263433b0723
Panayotis Katsaloulis <Panayotis Katsaloulis@localhost>
2013-10-15 03:43:05 +0300

    implement socket timeout for RMI

--- core/src/com/panayotis/jubler/rmi/JublerClient.java
+++ core/src/com/panayotis/jubler/rmi/JublerClient.java
@@ -21,9 +21,14 @@
 package com.panayotis.jubler.rmi;
 
 import com.panayotis.jubler.os.DEBUG;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMISocketFactory;
 
 /**
  *
@@ -38,6 +43,21 @@ public class JublerClient {
 
     public static boolean isRunning() {
         try {
+            RMISocketFactory.setSocketFactory(new RMISocketFactory() {
+                public Socket createSocket(String host, int port)
+                        throws IOException {
+                    Socket socket = new Socket();
+                    socket.setSoTimeout(3000);
+                    socket.setSoLinger(false, 0);
+                    socket.connect(new InetSocketAddress(host, port), 3000);
+                    return socket;
+                }
+
+                public ServerSocket createServerSocket(int port)
+                        throws IOException {
+                    return new ServerSocket(port);
+                }
+            });
             Registry registry = LocateRegistry.getRegistry(JublerServer.JUBLERPORT);
             stub = (JublerRMI) registry.lookup("Jubler");
             return true;
