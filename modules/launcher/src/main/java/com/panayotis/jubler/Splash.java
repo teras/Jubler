356b35ff8052984a2b2db52e5933a262ea041414
Panayotis Katsaloulis <panayotis@panayotis.com>
2023-12-14 00:17:28 +0200

    fix doc

--- modules/launcher/src/main/java/com/panayotis/jubler/Splash.java
+++ modules/launcher/src/main/java/com/panayotis/jubler/Splash.java
@@ -1,9 +1,6 @@
 /*
  * (c) 2005-2023 by Panayotis Katsaloulis
  * SPDX-License-Identifier: AGPL-3.0-only
-/*
- * (c) 2005-2023 by Panayotis Katsaloulis
- * SPDX-License-Identifier: AGPL-3.0-only
  * This file is part of Jubler.
  */
 
